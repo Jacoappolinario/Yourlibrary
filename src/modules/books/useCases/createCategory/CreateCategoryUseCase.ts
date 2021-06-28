5a6a09ea766657ad4976ade9237028bff4693ea8
Jacó Apolinário <jacoapollinario@gmail.com>
2021-06-28 14:54:41 -0300

    Yourlibrary Project

---
+++ src/modules/books/useCases/createCategory/CreateCategoryUseCase.ts
@@ -0,0 +1,22 @@
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute({ description, name }: IRequest): void {
+    const categoryAlreadyExits = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExits) {
+      throw new Error('Category already exists!');
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryUseCase };
